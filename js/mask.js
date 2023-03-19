ed313935c5647d075a48b578312c1f8dc788a7d3
kain74 <kain7402@naver.com>
2023-03-19 21:49:55 +0900

    스크립트 수정

--- js/mask.js
+++ js/mask.js
@@ -1,35 +1,38 @@
-const wrap = document.querySelector('main');
-const btns = wrap.querySelectorAll('#navi li');
-const panels = wrap.querySelectorAll('section article');
-const brs = wrap.querySelectorAll('br')
-const videos = wrap.querySelectorAll('video');
+windlow.addEventListener('load', function(){
+    const wrap = document.querySelector('main');
+    const btns = wrap.querySelectorAll('#navi li');
+    const panels = wrap.querySelectorAll('section article');
+    const brs = wrap.querySelectorAll('br')
+    const videos = wrap.querySelectorAll('video');
 
-btns.forEach((btn, index)=>{
-    btn.addEventListener('click', e =>{
-        for(let i=0; i<btns.length; i++){
-            btns[i].classList.remove('on');
-            btns[i].ariaSelected = false;
-            videos[i].pause();
-            if(panels[i].classList.contains('on')){
-                panels[i].classList.add('mask');
-            }
-        }
-        btns[index].classList.add('on');
-        btns[index].ariaSelected = true;
-        panels[index].classList.add('lower');
-        videos[index].play();
-        setTimeout(()=>{
-            for(let i=0; i<panels.length; i++){
+    btns.forEach((btn, index)=>{
+        btn.addEventListener('click', e =>{
+            for(let i=0; i<btns.length; i++){
+                btns[i].classList.remove('on');
+                btns[i].ariaSelected = false;
+                videos[i].pause();
                 if(panels[i].classList.contains('on')){
-                    panels[i].classList.remove('on');
-                    panels[i].classList.remove('mask');
+                    panels[i].classList.add('mask');
                 }
             }
-            panels[index].classList.remove('lower');
-            panels[index].classList.add('on');
-        },1400)
+            btns[index].classList.add('on');
+            btns[index].ariaSelected = true;
+            panels[index].classList.add('lower');
+            videos[index].play();
+            setTimeout(()=>{
+                for(let i=0; i<panels.length; i++){
+                    if(panels[i].classList.contains('on')){
+                        panels[i].classList.remove('on');
+                        panels[i].classList.remove('mask');
+                    }
+                }
+                panels[index].classList.remove('lower');
+                panels[index].classList.add('on');
+            },1400)
+        });
     });
 });
+
 
 
 
